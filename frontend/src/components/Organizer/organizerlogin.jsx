fa21028ea452dd953f9b0a0e9e4d7dc1d32b0700
chendrayasaravanan07-alt <chendrayasaravanan07@gmail.com>
2025-12-16 21:22:31 +0530

    Login

---
+++ frontend/src/components/Organizer/organizerlogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function organizerlogin() {
+  return (
+    <div>organizerlogin</div>
+  )
+}
+
+export default organizerlogin
---
+++ frontend/src/components/Student/studentlogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function studentlogin() {
+  return (
+    <div>studentlogin</div>
+  )
+}
+
+export default studentlogin
